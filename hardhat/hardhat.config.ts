1251ec63834c6f8fa2257dee6f4b82e7a2dffd41
Alison Haire <alihaire900@gmail.com>
2023-03-01 16:59:22 +0100

    Update hardhat.config.ts

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -7,6 +7,11 @@ import { resolve } from 'path';
 const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || './.env';
 dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
 
+const walletPrivateKey: string | undefined = process.env.WALLET_PRIVATE_KEY;
+if (!walletPrivateKey) {
+  throw new Error('Please set your Wallet private key in a .env file');
+}
+
 const config: HardhatUserConfig = {
   solidity: '0.8.17',
   defaultNetwork: 'filecoinHyperspace',
@@ -15,7 +20,7 @@ const config: HardhatUserConfig = {
     filecoinHyperspace: {
       url: 'https://api.hyperspace.node.glif.io/rpc/v1', //https://beryx.zondax.ch/ //chainstack
       chainId: 3141,
-      accounts: [process.env.WALLET_PRIVATE_KEY ?? 'undefined'],
+      accounts: [walletPrivateKey],
     },
   },
 };
