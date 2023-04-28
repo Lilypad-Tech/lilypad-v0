9bfa3ff5fa90c9e75167a084995a49a96e7587f7
Simon Worthington <simon@register-dynamics.co.uk>
2023-04-28 11:46:23 +0100

    Correctly configure logging (since Bac v.0.3.28).

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bacalhau-project/bacalhau/pkg/logger"
 	"github.com/bacalhau-project/lilypad/pkg/bridge"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -13,29 +14,34 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func main() {
-	ctx := log.Logger.WithContext(context.Background())
+func EnvOrDefault(env, defaultValue string) (value string) {
+	value, found := os.LookupEnv(env)
+	if !found {
+		value = defaultValue
+	}
+	return
+}
 
-	lvl := zerolog.InfoLevel
-	if level, found := os.LookupEnv("LOG_LEVEL"); found {
-		var err error
-		lvl, err = zerolog.ParseLevel(level)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err.Error())
-			return
-		}
+func main() {
+	logType, err := logger.ParseLogMode(EnvOrDefault("LOG_MODE", "default"))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return
+	}
+	logger.ConfigureLogging(logType)
+
+	lvl, err := zerolog.ParseLevel(EnvOrDefault("LOG_LEVEL", "INFO"))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return
 	}
 	zerolog.SetGlobalLevel(lvl)
 
+	ctx := log.Logger.WithContext(context.Background())
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
 	defer cancel()
 
-	sqliteFileLocation := os.Getenv("SQLITE_FILE_LOCATION")
-
-	if sqliteFileLocation == "" {
-		sqliteFileLocation = "lilypad.sqlite"
-	}
-
+	sqliteFileLocation := EnvOrDefault("SQLITE_FILE_LOCATION", "lilypad.sqlite")
 	repo, err := bridge.NewSQLiteRepository(ctx, sqliteFileLocation)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
